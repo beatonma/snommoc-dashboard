1ef33364289064deb4eff18303fbbc5db2617e07
Michael Beaton <beatonma@gmail.com>
2021-04-26 12:03:18 +0100

    Removed unneeded chunks generated by webpack-cli template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// Generated using webpack-cli http://github.com/webpack-cli
 const path = require('path');
 
 module.exports = {
@@ -18,10 +17,6 @@ module.exports = {
         open: true,
         host: 'localhost',
     },
-    plugins: [
-        // Add your plugins here
-        // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
-    ],
     module: {
         rules: [
             {
@@ -43,9 +38,6 @@ module.exports = {
                     }
                 }
             }
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
     resolve: {
