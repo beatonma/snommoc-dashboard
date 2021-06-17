f22cf6f1a67d3c016479bd7fcbca8c5b37c9087e
Michael Beaton <beatonma@gmail.com>
2021-06-17 15:30:47 +0100

    Converted Dashboard component class to function

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom";
 import "./scss/dashboard.scss";
 import SearchForm from "./search";
@@ -11,6 +11,7 @@ import RecentTasks, { CreateTaskActions } from "./tasks";
 import { Error, InlineError } from "./components/error";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { requestConfig } from "./util/actions";
+import { ActiveMembers } from "./members";
 
 function DashbaordApp() {
     ReactDom.render(<DashboardRouter />, document.getElementById("dashboard"));
@@ -28,110 +29,82 @@ function DashboardRouter() {
                     )}
                     component={Dashboard}
                 />
+
+                <Route
+                    path={Urls.activeMembers}
+                    exact
+                    component={ActiveMembers}
+                />
             </Switch>
         </Router>
     );
 }
 
-class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            zeitgeist: {},
-            error: null,
-            networkError: null,
-        };
+function Dashboard(props) {
+    const [zeitgeist, setZeitgeist] = useState({});
+    const [dirty, setDirty] = useState(false);
 
-        this.refreshZeitgeist = this.refreshZeitgeist.bind(this);
-        this.toggleFeatured = this.toggleFeatured.bind(this);
-        this.startTask = this.startTask.bind(this);
+    const refresh = () => setDirty(!dirty);
 
-        this.refreshZeitgeist();
-    }
-
-    static getDerivedStateFromError(error) {
-        return { error: error };
-    }
-
-    componentDidCatch(error, errorInfo) {
-        console.error(error);
-        console.error(errorInfo);
-    }
-
-    refreshZeitgeist() {
+    useEffect(() => {
         fetch(Urls.zeitgeist)
             .then(response => response.json())
-            .then(results => this.setState({ zeitgeist: results }))
-            .catch(err => this.setState({ networkError: err }));
-    }
+            .then(results => setZeitgeist(results));
+    }, [dirty]);
 
-    toggleFeatured(targetType, targetId, isFeatured) {
+    const toggleFeatured = (targetType, targetId, isFeatured) => {
         const url = Urls.actions.toggleFeatured(targetType, targetId);
         const requestType = isFeatured ? "DELETE" : "POST";
         const config = requestConfig(requestType);
 
-        fetch(url, config)
-            .then(response => this.refreshZeitgeist())
-            .catch(err => this.setState({ networkError: err }));
-    }
+        fetch(url, config).then(response => refresh());
+    };
 
-    startTask(url) {
-        fetch(url, requestConfig("POST"))
-            .then(response => console.log(url))
-            .catch(err => this.setState({ networkError: err }));
-    }
+    const startTask = url =>
+        fetch(url, requestConfig("POST")).then(response => console.log(url));
 
-    render() {
-        let content;
-
-        if (this.props.match?.params?.focussedUnlinkedConstituency) {
-            content = (
-                <UnlinkedConstituencyDetail
-                    id={this.props.match.params.focussedUnlinkedConstituency}
+    let content;
+    if (props.match?.params?.focussedUnlinkedConstituency) {
+        content = (
+            <UnlinkedConstituencyDetail
+                id={props.match.params.focussedUnlinkedConstituency}
+            />
+        );
+    } else {
+        content = (
+            <div>
+                <CreateTaskActions
+                    updateProfiles={() =>
+                        startTask(Urls.actions.tasks.update_profiles)
+                    }
+                    updatePortraits={() =>
+                        startTask(Urls.actions.tasks.update_portraits)
+                    }
+                    updateBills={() =>
+                        startTask(Urls.actions.tasks.update_bills)
+                    }
+                    updateDivisions={() =>
+                        startTask(Urls.actions.tasks.update_divisions)
+                    }
+                    updateElectionResults={() =>
+                        startTask(Urls.actions.tasks.update_election_results)
+                    }
                 />
-            );
-        } else {
-            content = (
-                <div>
-                    <CreateTaskActions
-                        updateProfiles={() =>
-                            this.startTask(Urls.actions.tasks.update_profiles)
-                        }
-                        updatePortraits={() =>
-                            this.startTask(Urls.actions.tasks.update_portraits)
-                        }
-                        updateBills={() =>
-                            this.startTask(Urls.actions.tasks.update_bills)
-                        }
-                        updateDivisions={() =>
-                            this.startTask(Urls.actions.tasks.update_divisions)
-                        }
-                        updateElectionResults={() =>
-                            this.startTask(
-                                Urls.actions.tasks.update_election_results
-                            )
-                        }
-                    />
-                    <UnlinkedConstituencies />
-                    <RecentTasks />
-                    <Zeitgeist
-                        zeitgeist={this.state.zeitgeist}
-                        toggleFeatured={this.toggleFeatured}
-                    />
-                </div>
-            );
-        }
-
-        return (
-            <DashboardChrome
-                toggleFeatured={this.toggleFeatured}
-                error={this.state.error}
-                networkError={this.state.networkError}
-            >
-                {content}
-            </DashboardChrome>
+                <UnlinkedConstituencies />
+                <RecentTasks />
+                <Zeitgeist
+                    zeitgeist={zeitgeist}
+                    toggleFeatured={toggleFeatured}
+                />
+            </div>
         );
     }
+
+    return (
+        <DashboardChrome toggleFeatured={toggleFeatured}>
+            {content}
+        </DashboardChrome>
+    );
 }
 
 /**
